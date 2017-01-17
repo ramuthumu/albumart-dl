e5e819dad8b979927d38cfabbd9c3842a8584ada
Ramu Thumu <ramuthumu@users.noreply.github.com>
2017-01-17 00:28:54 -0500

    added download

--- itunes.py
+++ itunes.py
@@ -1,5 +1,6 @@
 
-import requests, sys, pyperclip, bs4, webbrowser
+
+import requests, sys, pyperclip, os, shutil
 if len(sys.argv) > 1:
     album = ' '.join(sys.argv[1:])
 else:
@@ -9,5 +10,8 @@ else:
 res = requests.get('https://itunes.apple.com/search?term=%s&entity=album' % (album))
 res = res.json()
 url = res["results"][0]["artworkUrl100"].replace("100x100","600x600")
-
-webbrowser.open(url)
+name = res["results"][0]["collectionName"]
+image = requests.get(url, stream=True)
+with open(name + '.jpg', 'wb') as out_file:
+    shutil.copyfileobj(image.raw, out_file)
+del image
